d28f16e0c5218f6a2b75ab456dabdaa324c13f57
JPnatu <jpnatu2569@gmail.com>
2024-06-16 14:18:23 +0900

    cssの整理

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,40 +1,32 @@
 import React, { useState } from 'react';
-
-const albums = [
-  { id: 1, title: 'Album Title 1', artist: 'Artist 1', cover: 'https://via.placeholder.com/150', tracks: ['Track 1', 'Track 2', 'Track 3'] },
-  { id: 2, title: 'Album Title 2', artist: 'Artist 2', cover: 'https://via.placeholder.com/150', tracks: ['Track 1', 'Track 2'] },
-  { id: 3, title: 'Album Title 3', artist: 'Artist 3', cover: 'https://via.placeholder.com/150', tracks: ['Track 1', 'Track 2', 'Track 3', 'Track 4'] },
-  // さらにアルバムを追加
-];
+import Albums from "../assets/data/dummyAlbums.json";
 
 const MainInterface = () => {
   const [selectedAlbum, setSelectedAlbum] = useState(null);
 
-  const toggleAlbumDetails = (album) => {
-    if (selectedAlbum && selectedAlbum.id === album.id) {
+  const toggleAlbumDetails = (Albums) => {
+    if (selectedAlbum && selectedAlbum.id === Albums.id) {
       setSelectedAlbum(null);
     } else {
-      setSelectedAlbum(album);
+      setSelectedAlbum(Albums);
     }
   };
 
   return (
     <div className="flex h-full">
-      <div className="flex-grow p-8 bg-gray-100 overflow-y-auto">
-        <section id="library">
-          <h2 className="text-xl mb-4">Library</h2>
+      <div className="pt-20 pb-24 p-4 overflow-y-auto">
+          <h2 className="title">Library</h2>
           {/* Album list */}
-          <div className="grid grid-cols-1 sm:grid-cols-3 
-                          md:grid-cols-4 lg:grid-cols-6 gap-5">
-            {albums.map(album => (
-              <div key={album.id} className="bg-gray-800 p-2 rounded" onClick={() => toggleAlbumDetails(album)}>
-                <img src={album.cover} alt={album.title} className="w-full h-auto rounded-md" />
-                <h3 className="mt-2 text-lg">{album.title}</h3>
-                <p>{album.artist}</p>
+          <div className="shelf">
+            {Albums.map((album, index)=> (
+              <div className="album" key={index} onClick={() => toggleAlbumDetails(album)}>
+                <img className="album-image"
+                  src={album.cover}alt={`${album.title} cover`}/>
+                <p className="whitespace-nowrap overflow-hidden text-white">{album.title}</p>
+                <p className="text-sm text-gray-300">{album.date}</p>
               </div>
             ))}
           </div>
-        </section>
       </div>
 
       {selectedAlbum && (
